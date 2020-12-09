43ddb1962f0f8f18b5c86389414c299a3ef00f2d
Paweł Kusiński <KusinskiP@protonmail.com>
2020-12-09 17:29:41 +0000

    Update web_run

--- payload_generator/web_run.py
+++ payload_generator/web_run.py
@@ -6,12 +6,12 @@ from .payload_generator import PayloadGenerator
 from common.logger import setup_logger, log_level
 
 
-def web_run(target_domain, forgery_domain, list_path, list_name, log_path):
+def web_run(target_domain, forgery_domain, list_path, list_name, log_path, protocols=['http','https','dict'], ports=['22', '80', '443']):
     if not os.path.isdir(log_path):
         os.mkdir(log_path)
     if not os.path.isdir(list_path):
         os.mkdir(list_path)
 
     setup_logger(log_level(3), f"{log_path}/ssrf_payloads.log")
-    pg = PayloadGenerator(target_domain, forgery_domain, save_path=list_path, payload_filename=list_name)
+    pg = PayloadGenerator(target_domain, forgery_domain, save_path=list_path, payload_filename=list_name, protocols=protocols, ports=ports)
     pg.run()
